7e56b3710b49edec429842b486d3611630e3e288
philipgreat <philip_chang@163.com>
2015-11-06 09:17:09 +0800

    Update reactize.js

--- reactize.js
+++ reactize.js
@@ -1,11 +1,11 @@
-	// console.log("node1: "+node.nodeType +" -->\n\t:"+node);
-	// create a component named <componentName> and put the text into
-	// renders;
-	/*
-	 * var ExampleApplication = React.createClass({ render: function() {
-	 * 
-	 * return <p>{message}</p>; } });
-	 */
+// console.log("node1: "+node.nodeType +" -->\n\t:"+node);
+// create a component named <componentName> and put the text into
+// renders;
+/*
+ * var ExampleApplication = React.createClass({ render: function() {
+ * 
+ * return <p>{message}</p>; } });
+ */
 
 var fs = require('fs');
 
@@ -26,40 +26,89 @@ var reactizeNode = function(node) {
 		return;
 	}
 	// this is an element node
+	replaceToClassName(node);
+
+	var reactComponentName = replaceToReactNode(node);
+	if (!reactComponentName) {
+		return;
+	}
+	exportComponentToFile(reactComponentName,node);
+}
+
+var replaceToClassName = function(node) {
+	var className = node.getAttribute("class");
+	if (className) {
+		node.removeAttribute("class");
+		node.setAttribute("className", className);
+		// return;
+	}
+}
+
+var replaceToReactNode = function(node) {
 
 	var componentName = node.getAttribute("rc");
 	if (!componentName) {
 		return;
 	}
 	node.removeAttribute("rc");
-
 	node.parentNode.removeChild(node);// removeChild
-	var rootDoc=getRootDocument(node);
+	var rootDoc = getRootDocument(node);
 	var childComponent = rootDoc.createElement(componentName);
 	node.parentNode.appendChild(childComponent);
+	return componentName;
 
-	var componentCode = "var " + componentName + " = React.createClass({\n"
+}
+/*'use strict';
+
+var React = require('react');*/
+
+
+var exportComponent = function(componentName, node) {
+
+	var componentCode = "'use strict'\n"
+			+"var React = require('react');\n"
+			+"var " + componentName + " = React.createClass({\n"
 			+ "\trender: function() {\n" + "\t\treturn (" + node + ");\n"
 			+ "\t}\n" + "});\n"
 	console.log(componentCode);
+
 }
 
-var getRootDocument = function(node){
-	var parentNode=node.parentNode;
+var exportComponentToFile = function(componentName, node) {
 
-  if(!parentNode){
-			return node;
+	var componentCode = "'use strict'\n"
+			+"var React = require('react');\n"
+			+"var " + componentName + " = React.createClass({\n"
+			+ "\trender: function() {\n" + "\t\treturn (" + node + ");\n"
+			+ "\t}\n" + "});\n"
+			+"module.exports = "+componentName+";"
+	
+	var fileName = componentName + ".React.js";
+	fs.writeFile(fileName, componentCode, function(err) {
+		if (err){
+			throw err;			
+		}
+		console.log("------------------------"+fileName+"------------------------------");
+		console.log(componentCode);
+		console.log("========================"+fileName+"================================");
+	});
+}
+
+var getRootDocument = function(node) {
+	var parentNode = node.parentNode;
+
+	if (!parentNode) {
+		return node;
 	}
-	while(true){
-		
-		if(!parentNode.parentNode){
+	while (true) {
+
+		if (!parentNode.parentNode) {
 			return parentNode;
 		}
-		var parentNode=parentNode.parentNode;
+		var parentNode = parentNode.parentNode;
 	}
-	
-}
 
+}
 
 var visiteNode = function(node, postCallback, preCallback) {
 	if (!node) {
@@ -95,11 +144,11 @@ var reactize = function(xmlText) {
 
 	var DOMParser = require('xmldom').DOMParser;
 	var doc = new DOMParser().parseFromString(xmlText, 'text/xml');
-	//console.log(typeof(doc));
-	//var firstElement = doc.getElementsByTagName('div')[0]
+	// console.log(typeof(doc));
+	// var firstElement = doc.getElementsByTagName('div')[0]
 
-	//var XMLSerializer = require('xmldom').XMLSerializer;
-	//var s = new XMLSerializer();
+	// var XMLSerializer = require('xmldom').XMLSerializer;
+	// var s = new XMLSerializer();
 
 	// console.info(s.serializeToString(doc));
 
